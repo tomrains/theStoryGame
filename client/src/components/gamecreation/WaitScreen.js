c4c40ce00558dc291395442fab90e82b8b0b506e
Tom Rains <tomrainswrites@gmail.com>
2020-08-25 21:41:08 -0500

    Fix get bug

--- client/src/components/gamecreation/WaitScreen.js
+++ client/src/components/gamecreation/WaitScreen.js
@@ -55,7 +55,7 @@ class WaitScreen extends React.Component {
     if (this.props.gameId) {
       console.log(`This.props.gameId is: ${this.props.gameId}`);
       // axios.get(`http://localhost:4000/games/${this.props.gameId}/players`)
-      axios.get(`api/players/${this.props.gameId}/players`)
+      axios.get(`api/players/${this.props.gameId}/player`)
         // .then(game => this.setState({ gameInfo: game.data[0].players }));
         .then(game => this.setState({
           gameInfo: game.data[0].players,
