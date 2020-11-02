5fd85f442a55f82526d0b5467f31b36cc31c8d21
Tom Rains <tomrainswrites@gmail.com>
2020-11-02 14:12:29 -0600

    push it

---
+++ client/build/precache-manifest.a23280d73186f6ad06436e1f1f89d8e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d984efbdac4bb16e1e4e85a991646da",
+    "url": "/theStoryGame/index.html"
+  },
+  {
+    "revision": "484e40699507d15062f4",
+    "url": "/theStoryGame/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "345911f72bc00709a4ba",
+    "url": "/theStoryGame/static/css/main.f2895dc2.chunk.css"
+  },
+  {
+    "revision": "484e40699507d15062f4",
+    "url": "/theStoryGame/static/js/2.ee13aea0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/theStoryGame/static/js/2.ee13aea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345911f72bc00709a4ba",
+    "url": "/theStoryGame/static/js/main.18db8343.chunk.js"
+  },
+  {
+    "revision": "36922fd09f1d84c75544",
+    "url": "/theStoryGame/static/js/runtime-main.6107d3eb.js"
+  }
+]);
